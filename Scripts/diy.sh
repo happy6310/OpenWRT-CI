76ed8e5847c166cad5a8399d15f3efe4c7d13b60
happyliu <happy6310@qq.com>
2026-03-15 18:20:40 +0800

    依赖梳理

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -68,19 +68,34 @@ UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-25.12"
 
 # IPsec 服务器
 UPDATE_PACKAGE "luci-app-ipsec-server" "Ivaneus/luci-app-ipsec-server" "main"
-# 网络测试 speedtest-cli
-UPDATE_PACKAGE "speedtest-cli" "https://github.com/sbwml/openwrt_pkgs.git" "main" "pkg"
-UPDATE_PACKAGE "luci-app-netspeedtest" "https://github.com/sbwml/openwrt_pkgs.git" "main" "pkg"
+
+
+# 网络测试 speedtest-cli 以及 vlmcsd
+UPDATE_PACKAGE "speedtest-cli luci-app-netspeedtest \
+                 vlmcsd luci-app-vlmcsd \
+                 luci-app-socat \
+                 " "https://github.com/sbwml/openwrt_pkgs.git" "main" "pkg"
+
+UPDATE_PACKAGE "cups luci-app-cupsd \
+                 vlmcsd luci-app-vlmcsd \
+                 " "https://github.com/fichenx/openwrt-package" "main" "pkg"
+
+
+
 # 带宽监控 bandix
 UPDATE_PACKAGE "openwrt-bandix" "timsaya/openwrt-bandix" "main"
 UPDATE_PACKAGE "luci-app-bandix" "timsaya/luci-app-bandix" "main"
 
 # 直接安装所需的应用
 UPDATE_PACKAGE "luci-app-openclash" "vernesong/OpenClash" "dev" "pkg"
-# 端口转发 socat
-UPDATE_PACKAGE "luci-app-socat" "https://github.com/sbwml/luci-app-socat" "main"
+
 # WOL Plus
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/packages" "main"
+
+# Athena LED
+UPDATE_PACKAGE "luci-app-athena-led" "Sh1rokoDev/luci-app-athena-led" "LuCI2-JS"
+
+
 
 # 配置项
 provided_config_lines=(
@@ -96,20 +111,24 @@ provided_config_lines=(
     "CONFIG_PACKAGE_unzip=y"
     "CONFIG_PACKAGE_kmod-nft-tproxy=y"
     "CONFIG_PACKAGE_luci-compat=y"
-    #########
-
+    ######## ttyd
+    "CONFIG_PACKAGE_ttyd=y"
+    "CONFIG_PACKAGE_luci-app-ttyd=y"
+    "CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y"
+    ######## wireguard
+    "CONFIG_PACKAGE_kmod-wireguard=y"
+    "CONFIG_PACKAGE_wireguard-tools=y"
+    "CONFIG_PACKAGE_luci-proto-wireguard=y"
+    ######## 
     "CONFIG_PACKAGE_luci-app-ipsec-server=y"
     "CONFIG_PACKAGE_luci-app-socat=y"
     "CONFIG_PACKAGE_luci-app-openclash=y"
     "CONFIG_PACKAGE_luci-app-bandix=y"
     "CONFIG_PACKAGE_luci-app-wolplus=y"
     "CONFIG_PACKAGE_luci-app-autoreboot=y"
-    "CONFIG_PACKAGE_luci-app-ttyd=y"
-    "CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y"
-    "CONFIG_PACKAGE_ttyd=y"
-    "CONFIG_PACKAGE_kmod-wireguard=y"
-    "CONFIG_PACKAGE_wireguard-tools=y"
-    "CONFIG_PACKAGE_luci-proto-wireguard=y"
+    "CONFIG_PACKAGE_luci-app-cupsd=y"
+    "CONFIG_PACKAGE_luci-app-vlmcsd=y"
+    "CONFIG_PACKAGE_luci-app-netspeedtest=y"
     #"CONFIG_PACKAGE_ddns-scripts=y"
     #"CONFIG_PACKAGE_ddns-scripts_cloudflare.com-v4=y"
 )
@@ -143,6 +162,16 @@ chmod +x clash_meta
 rm -f clash-linux-arm64.tar.gz
 EOF
 chmod +x package/base-files/files/etc/uci-defaults/99-openclash-core
+
+
+# 为 ax6600 追加配置
+[[ $WRT_CONFIG == "ax6600"* ]] && provided_config_lines+=(
+    "CONFIG_PACKAGE_sqm-scripts-nss=y"
+    "CONFIG_PACKAGE_luci-app-sqm=y"
+    "CONFIG_PACKAGE_luci-i18n-sqm-zh-cn=y"
+    "CONFIG_PACKAGE_luci-app-athena-led=y"
+)
+
 
 
 # 安装 Go
